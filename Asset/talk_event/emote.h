fa126b13ad05bf4d80560dd3c188e9d76d57ee91
hellacopters <salpesce@gmail.com>
2023-11-01 02:32:35 -0400

    first commit

---
+++ Asset/talk_event/emote.h
@@ -0,0 +1,15 @@
+typedef enum <int> {
+	Fusy = 1,
+	Sweat = 2,
+	Cry = 3,
+	Diamonds = 4,
+	Heart = 5,
+	Zzz = 6,
+	Exclamation7 = 7,
+	Exclamation8 = 8,
+	Question = 9,
+	Dots = 10,
+	Music_Note = 11,
+	Frustrated = 12,
+	Angry = 13
+} EMOTE;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Opera Omnia Game File Templates
+
+010 Editor templates for Opera Omnia game files
